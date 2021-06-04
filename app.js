5288c7ebfe843d8af1c743872521872c261dce0c
$udam <48345668+gdsghost@users.noreply.github.com>
2021-06-04 20:28:23 +0530

    part 04 Connect to MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 //express app
 const app = express();
 
+//connect to mongoDB
+const dbURL = 'mongodb+srv://sudam:test123@nodetute.nnrrx.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine','ejs');
-
-//listen to requests
-app.listen(3000);
 
 // middleware & static files - loading stylesheet
 app.use(express.static('public'));
@@ -16,21 +22,45 @@ app.use(express.static('public'));
 // 3rd party middleware
 app.use(morgan('dev'));
 
-//Custome middlewear
-app.use((req,res,next) => {
-    console.log('new request made:');
-    console.log('host:',req.hostname);
-    console.log('path:',req.path);
-    console.log('method:',req.method);
-    next();
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req,res)=>{
+    const blog = new Blog({
+        title:'new blog 2',
+        snippet: "about my new blog 2",
+        body:'more about my new blog 2'
+    });
+
+    blog.save()
+       .then((result)=>{
+           res.send(result)
+       })
+       .catch((err) => {
+           console.log(err);
+       })
 });
 
-// another custom middleware
-app.use((req,res,next) => {
-    console.log('in the next middleware');
-    next();
+app.get('/all-blogs',(req,res) => {
+    Blog.find()
+      .then((result) => {
+          res.send(result);
+      })
+      .catch((err) => {
+          console.log(err);
+      });
 });
 
+app.get('/single-blog', (req,res)=>{
+    Blog.findById('60ba3c94c712412e3c95f8f3')
+     .then((result) => {
+         res.send(result)
+     })
+     .catch((err) => {
+         console.log(err);
+     })
+})
+
+
+//routes
 app.get('/',(req,res) => {
     const blogs = [
         {title:'Getting started with AR', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae.'},
