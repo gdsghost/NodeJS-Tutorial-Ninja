d78dde7f324173ba303c8628feefef8e9d2f3965
$udam <48345668+gdsghost@users.noreply.github.com>
2021-06-07 21:18:57 +0530

    MVC pattern and styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 
 //express app
@@ -9,80 +9,37 @@ const app = express();
 
 //connect to mongoDB
 const dbURL = 'mongodb+srv://sudam:test123@nodetute.nnrrx.mongodb.net/node-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
 
 //register view engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 // middleware & static files - loading stylesheet
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // 3rd party middleware
 app.use(morgan('dev'));
 
-//mongoose and mongo sandbox routes
-app.get('/add-blog', (req,res)=>{
-    const blog = new Blog({
-        title:'new blog 2',
-        snippet: "about my new blog 2",
-        body:'more about my new blog 2'
-    });
-
-    blog.save()
-       .then((result)=>{
-           res.send(result)
-       })
-       .catch((err) => {
-           console.log(err);
-       })
-});
-
-app.get('/all-blogs',(req,res) => {
-    Blog.find()
-      .then((result) => {
-          res.send(result);
-      })
-      .catch((err) => {
-          console.log(err);
-      });
-});
-
-app.get('/single-blog', (req,res)=>{
-    Blog.findById('60ba3c94c712412e3c95f8f3')
-     .then((result) => {
-         res.send(result)
-     })
-     .catch((err) => {
-         console.log(err);
-     })
-})
-
 
 //routes
-app.get('/',(req,res) => {
-    const blogs = [
-        {title:'Getting started with AR', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae.'},
-        {title:'Getting started with VR', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae.'},
-        {title:'Getting started with .Net', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae.'}
-    ];
-    //res.send('<p>Home Page</p>');
-    res.render('index',{title:'Home', blogs});
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
 });
 
-app.get('/about',(req,res) => {
+app.get('/about', (req, res) => {
     //res.send('<p>About Page</p>');
-    res.render('about',{title:'About'});
+    res.render('about', { title: 'About' });
 });
 
 
-app.get('/blog/create',(req,res) => {
-    res.render('create',{title:'Create'});
-});
+//blog routes
+app.use('/blogs', blogRoutes);
 
 
 // 404 page
-app.use((req,res) => {
-    res.status(404).render('404',{title:'404'});
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
 });
